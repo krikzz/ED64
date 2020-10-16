03a655545fd4551addd961bf36bf4a306c8aa1d8
Robin Jones <networkfusion@users.noreply.github.com>
2020-10-16 23:41:36 +0100

    Add a unit test project

---
+++ usb64/UnitTests/CommadProcessorTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CommadProcessorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
