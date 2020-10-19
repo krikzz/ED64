74bc48ac8b67a28bb274007355ee72d0a0283325
Robin Jones <networkfusion@users.noreply.github.com>
2020-10-19 21:29:05 +0100

    Add ( as yet unused) interfaces for different carts and OS

---
+++ usb64/usb64/VSeriesOs2OfficialInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ed64usb
+{
+    public class VSeriesOs2OfficialInterface
+    {
+        public void LoadRom()
+        {
+
+        }
+
+        public void StartRom()
+        {
+
+        }
+
+    }
+}
---
+++ usb64/usb64/XSeriesOs3Interface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ed64usb
+{
+    public class XSeriesOs3Interface
+    {
+        public void LoadRom()
+        {
+
+        }
+
+        public void StartRom()
+        {
+
+        }
+
+        public void DumpScreenBuffer()
+        {
+
+        }
+    }
+}
